db0e9355fbcef05dd915d2b4e14f4ff940f1b533
@rikatussei <yohei.arisu@Gmail.com>
2024-03-21 15:24:51 +0900

    ユーザーテーブル作成

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ gem 'pry-rails'
 group :development do
   gem 'rubocop', require: false
 end
+
+gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_21_025414) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_21_061137) do
   create_table "prototypes", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "catch_copy"
@@ -19,6 +19,22 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_21_025414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_prototypes_on_user_id"
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
