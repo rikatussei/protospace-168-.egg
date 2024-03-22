425185426fc054341e2708b03650f457f2c26d59
@rikatussei <yohei.arisu@Gmail.com>
2024-03-22 19:42:15 +0900

    プロトタイプモデルにバリデーションを設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,4 +3,9 @@ class Prototype < ApplicationRecord
   belongs to :user
 
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
 end
