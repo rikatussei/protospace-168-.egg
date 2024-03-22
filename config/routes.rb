0afd37ea2613297e03c383e3a5282dde54d8948e
@rikatussei <yohei.arisu@Gmail.com>
2024-03-22 16:18:34 +0900

    トップページへの遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes
+  resources :prototypes, only: [:index, :new]
   resources :users, only: [:index, :new, :edit, :update]
-  # resources :tweets, only: [:index, :new]
+
 end
