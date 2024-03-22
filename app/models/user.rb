7629cb01f86947f6a804b1fd06cf0d070fcbdca1
beppu2024 <162876356+beppu2024@users.noreply.github.com>
2024-03-22 16:52:29 +0900

    バリデーションの設定２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true, 
-         validates :profile,
-         validates :occupation,
-         validates :position,
+         validates :name, presence: true
+         validates :profile, presence: true
+         validates :occupation, presence: true
+         validates :position, presence: true
 
 end
