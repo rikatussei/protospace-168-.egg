8c1534d339c3281ae1eeed673a93c1be7f7d8765
@rikatussei <yohei.arisu@Gmail.com>
2024-03-26 15:01:21 +0900

    空のコメントを入力してもエラーが発生しないようにしました。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,7 +22,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments
+    @comments = @prototype.comments if @prototype
   end
 
   def edit
